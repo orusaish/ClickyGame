eb161b7a220b8d779a9fb4f994a3058a1e77e4ef
Orusa Ishtiaq <orusaishtiaq@gmail.com>
2019-06-22 17:54:14 -0400

    Updates

---
+++ precache-manifest.5e0e981ff393eb1b302493ccd2558b10.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5151cb47aadb8c3d847eacade25e135b",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "0480c8085800ff29fe60",
+    "url": "/ClickyGame/static/css/main.5243c0bf.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "0480c8085800ff29fe60",
+    "url": "/ClickyGame/static/js/main.56e99813.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "c0405543759ee9c367e4d0e8cb65dda0",
+    "url": "/ClickyGame/static/media/homeScreen.c0405543.png"
+  },
+  {
+    "revision": "509e9e03dc82c51a9b5c7795f8d2b21c",
+    "url": "/ClickyGame/static/media/pic1.509e9e03.jpg"
+  },
+  {
+    "revision": "cdd7f1aa1a205b20e99360fd430c4849",
+    "url": "/ClickyGame/static/media/pic10.cdd7f1aa.jpg"
+  },
+  {
+    "revision": "abb6029b98effb8ce8f470a708557a48",
+    "url": "/ClickyGame/static/media/pic2.abb6029b.jpg"
+  },
+  {
+    "revision": "43d9cebd585ed005b4d333b1e2011d7b",
+    "url": "/ClickyGame/static/media/pic3.43d9cebd.jpg"
+  },
+  {
+    "revision": "6f9cbc27595b04dca213b09f376761b0",
+    "url": "/ClickyGame/static/media/pic4.6f9cbc27.jpg"
+  },
+  {
+    "revision": "0a33aff1ff58f929fcc3e9b1bb8f392a",
+    "url": "/ClickyGame/static/media/pic5.0a33aff1.jpg"
+  },
+  {
+    "revision": "6f84bd0b8108ae9a6692fb4af07efac2",
+    "url": "/ClickyGame/static/media/pic6.6f84bd0b.jpg"
+  },
+  {
+    "revision": "f1c0086cf0ff1822f480cd2b5889353c",
+    "url": "/ClickyGame/static/media/pic7.f1c0086c.png"
+  },
+  {
+    "revision": "39259feb0f5feeac48cc55791ce09441",
+    "url": "/ClickyGame/static/media/pic8.39259feb.jpg"
+  },
+  {
+    "revision": "958dd5536547a5fc8934fc1281592a5b",
+    "url": "/ClickyGame/static/media/pic9.958dd553.jpg"
+  }
+]);
